1c55b1dfa17848433795f29f5ad5a71812fa3e02
techTomita <tech.tomita@gmail.com>
2022-06-06 01:53:48 +0000

    sessions#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,32 @@ class SessionsController < ApplicationController
   end
 
   def create
+    email = params[:session][:email].downcase
+    password = params[:session][:password]
+    
+    if login(email, password)
+      flash[:success] = "ログインに成功しました。"
+      redirect_to @user
+    else
+      flash.now[:danger] = "ログインに失敗しました。"
+      render :new, status: :unprocessable_entity #status: :unprocessable_entity rails7.0以降追記が必要
+    end
   end
 
   def destroy
+  end
+  
+  
+  private
+  
+  def login(email, password)
+    @user = User.find_by(email: email)
+    
+    if @user && @user.authenticate(password)
+      session[:user_id] = @user.id
+      return true
+    else
+      return false
+    end
   end
 end
