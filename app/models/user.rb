81b4b82d2b2ee336831a39808a662f6160a70dd2
techTomita <tech.tomita@gmail.com>
2022-06-06 07:25:06 +0000

    generate model Relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
     has_many :microposts
+    has_many :relationships
+    has_many :followings, through: :relationships, source: :follow
+    has_many :reverse_of_relationship, class_name: "Relationship", foreign_key: "follow_id"
+    has_many :followers, through: :reverses_of_relatinship, source: :user
     
     validates :name, presence: true, length: {maximum: 50}
     validates :email, presence: true, length: {maximum: 255}, format: {with: /\A[\w\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}, uniqueness: {case_sensitive: false}
