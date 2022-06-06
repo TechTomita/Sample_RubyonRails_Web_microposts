a35660addb417b040227fbf9401d7f1d06819080
techTomita <tech.tomita@gmail.com>
2022-06-06 07:47:00 +0000

    add method follw(), unfollow()

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
     has_many :relationships
     has_many :followings, through: :relationships, source: :follow
     has_many :reverse_of_relationship, class_name: "Relationship", foreign_key: "follow_id"
-    has_many :followers, through: :reverses_of_relatinship, source: :user
+    has_many :followers, through: :reverse_of_relationship, source: :user
     
     validates :name, presence: true, length: {maximum: 50}
     validates :email, presence: true, length: {maximum: 255}, format: {with: /\A[\w\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}, uniqueness: {case_sensitive: false}
@@ -11,4 +11,19 @@ class User < ApplicationRecord
     before_save {self.email.downcase!}
     
     has_secure_password
+    
+    def follow(other_user)
+        unless self == other_user
+            self.relationships.find_or_create_by(follow_id: other_user.id) #Relationshipのインスタンスを返す
+        end
+    end
+    
+    def unfollow(other_user)
+        relationship = self.relationships.find_by(follow_id: other_user.id)
+        relationship.destroy if relationship
+    end
+    
+    def following?(other_user)
+        self.followings.include?(other_user)
+    end
 end
