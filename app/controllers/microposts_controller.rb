85ffece97748b7fa8fa8928e5e9df0e0a775b085
techTomita <tech.tomita@gmail.com>
2022-06-06 05:49:40 +0000

    microposts#create

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,7 +1,26 @@
 class MicropostsController < ApplicationController
+  before_action :require_user_logged_in
+  
   def create
+    @micropost = current_user.microposts.build(micropost_params)
+    
+    if @micropost.save
+      flash[:success] = "メッセージを投稿しました。"
+      redirect_to root_url
+    else
+      @pagy, @microposts = pagy(current_user.microposts.order(id: :desc)) #toppages/indexはアクションの処理をせずにtoppage.html.erbを返すだけのためインスタンスが必要
+      flash.now[:danger] = "メッセージの投稿に失敗しました。"
+      render "toppages/index", status: :unprocessable_entity #status: :unprocessable_entity rails7.0以降追記が必要
+    end
   end
 
   def destroy
+  end
+  
+  
+  private
+  
+  def micropost_params
+    params.require(:micropost).permit(:content)
   end
 end
