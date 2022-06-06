31f9c48d2cd4a33f4964020f33a40a2537904f23
techTomita <tech.tomita@gmail.com>
2022-06-06 01:25:51 +0000

    users#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,5 +12,21 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = "ユーザを登録しました。"
+      redirect_to @user
+    else
+      flash.now[:danger] = "ユーザの登録に失敗しました。"
+      render :new, status: :unprocessable_entity #status: :unprocessable_entity rails7.0以降追記が必要
+    end
+  end
+  
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
