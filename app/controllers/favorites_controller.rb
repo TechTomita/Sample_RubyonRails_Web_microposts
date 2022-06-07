b594c629b18eda06323cabc31676ec9261836ebd
techTomita <tech.tomita@gmail.com>
2022-06-07 00:07:01 +0000

    favorites#create

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,6 +2,11 @@ class FavoritesController < ApplicationController
   before_action :require_user_logged_in
   
   def create
+    fav_post = Micropost.find(params[:post_id])
+    
+    current_user.favorite(fav_post)
+    flash[:success] = "お気に入り登録しました。"
+    redirect_to root_path #likes_pathに変更する
   end
 
   def destroy
